6ac73061df8b1b316df10668329bb2b6b9d9ee67
Frexor <mateuszj93@gmail.com>
2022-04-20 18:19:11 +0200

    1. Implement Dependency Injection Framework.

--- CreditApplication/src/main/java/pl/javaskills/creditapp/Main.java
+++ CreditApplication/src/main/java/pl/javaskills/creditapp/Main.java
@@ -2,29 +2,31 @@ package pl.javaskills.creditapp;
 
 import pl.javaskills.creditapp.client.CreditApplicationReader;
 import pl.javaskills.creditapp.client.DummyCreditApplicationReader;
-import pl.javaskills.creditapp.core.*;
-import pl.javaskills.creditapp.core.scoring.EducationCalculator;
-import pl.javaskills.creditapp.core.scoring.GuarantorsCalculator;
-import pl.javaskills.creditapp.core.scoring.IncomeCalculator;
-import pl.javaskills.creditapp.core.scoring.MaritalStatusCalculator;
-import pl.javaskills.creditapp.core.validation.*;
+import pl.javaskills.creditapp.core.CreditApplicationManager;
+import pl.javaskills.creditapp.core.validation.CompoundPostValidator;
+import pl.javaskills.creditapp.core.validation.ExpansesPostValidator;
+import pl.javaskills.creditapp.core.validation.ObjectValidator;
+import pl.javaskills.creditapp.core.validation.PurposeOfLoanPostValidator;
+import pl.javaskills.creditapp.core.validation.reflection.*;
+import pl.javaskills.creditapp.di.ClassInitializer;
+
+import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         CreditApplicationReader reader = new DummyCreditApplicationReader();
-        EducationCalculator educationCalculator = new EducationCalculator();
-        MaritalStatusCalculator maritalStatusCalculator = new MaritalStatusCalculator();
-        IncomeCalculator incomeCalculator = new IncomeCalculator();
-        SelfEmployedScoringCalculator selfEmployedScoringCalculator = new SelfEmployedScoringCalculator();
-        GuarantorsCalculator guarantorsCalculator = new GuarantorsCalculator();
-        PersonScoringCalculatorFactory personScoringCalculatorFactory = new PersonScoringCalculatorFactory(selfEmployedScoringCalculator, educationCalculator, maritalStatusCalculator, incomeCalculator, guarantorsCalculator);
-        GuarantorValidator guarantorValidator = new GuarantorValidator();
-        CreditApplicationValidator creditApplicationValidator = new CreditApplicationValidator(new PersonValidator(new PersonalDataValidator()), new PurposeOfLoanValidator(), guarantorValidator);
-        CompoundPostValidator compoundPostValidator = new CompoundPostValidator(new PurposeOfLoanPostValidator(), new ExpansesPostValidator());
-        CreditApplicationService service = new CreditApplicationService(personScoringCalculatorFactory, new CreditRatingCalculator(), creditApplicationValidator, compoundPostValidator);
-        CreditApplicationManager manager = new CreditApplicationManager(service);
 
+        List<FieldAnnotationProcessor> fieldProcessors = List.of(new NotNullAnnotationProcessor(), new RegexAnnotationProcessor());
+        List<ClassAnnotationProcessor> classProcessors = List.of(new ExactlyOneNotNullAnnotationProcessor());
+        final ObjectValidator objectValidator = new ObjectValidator(fieldProcessors, classProcessors);
+
+        CompoundPostValidator compoundPostValidator = new CompoundPostValidator(new PurposeOfLoanPostValidator(), new ExpansesPostValidator());
+        ClassInitializer classInitializer = new ClassInitializer();
+        classInitializer.registerInstance(compoundPostValidator);
+        classInitializer.registerInstance(objectValidator);
+
+        CreditApplicationManager manager = (CreditApplicationManager) classInitializer.createInstance(CreditApplicationManager.class);
         manager.add(reader.read());
 
         manager.startProcessing();
